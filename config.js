a13c2760a5d9d99eb3560bb346d4836e92bc02ed
ntscas <ntscas@gmail.com>
2026-06-17 01:26:01 +0900

    Update config.js

--- config.js
+++ config.js
@@ -1,9 +1,5 @@
-// config.js
-
-
-
-
-
-
+const GOOGLE_API_KEY = 'AIzaSyBS1kIHPn1j2iCL4npiitSo_6Sm0M42450';
+const GOOGLE_SHEET_ID = '1CtvvJmwuEw1LA9GjGe1RncKMTdECQxwU-CVFkj122iA';
+const SHEET_RANGE = 'Tax_Hub!A1:O'; 
 
 
